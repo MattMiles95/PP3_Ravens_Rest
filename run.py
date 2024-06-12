4678eb4391775bc0b00c60ca1d03fbaa7cd64d37
Matt Miles <matt_miles2@live.co.uk>
2024-06-12 15:24:42 +0000

    Add comments to existing functions and format text layout

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from pyfiglet import figlet_format
-from pprint import pprint
 import time
 import random
+from colorama import init, Fore, Style
 
+# Initialise colorama
+init(autoreset=True)
+
+# Player Charater Option 1
 lee_kennedy = {
     "Name": "Lee Kennedy",
     "Health": 100,
@@ -11,22 +15,30 @@ lee_kennedy = {
     "Inventory": []
 }
 
+# Player Charater Option 1
 claire_greenfield = {
     "Name": "Claire Greenfield",
-    "Health": 100,
+    "Health": 120,
     "Attack Power": 20,
     "Weapon": "Unarmed",
     "Inventory": []
 }
 
+# Populated once Player Character selected
 player_card = {}
 
 def type_text(text):
+    """
+    Prints text one character at a time to create a 'typing' animation.
+    """
     for char in text:
         print(char, end='', flush=True)
-        time.sleep(random.uniform(0.001, 0.1))
+        time.sleep(random.uniform(0.001, 0.09))
 
 def main_menu():
+    """
+    The menu the User is presented with when opening the app.
+    """
     raven_image = r"""
                       ⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡟⠋⢻⣷⣄⡀⠀⠀⠀⠀⠀
@@ -68,7 +80,8 @@ def main_menu():
     intro = ("\nWelcome to Raven's Rest, a text-based adventure game!" 
     "\nI'm Poe, your digital Game Master (or 'GM' for short)." 
     "\nType out what you want to do, and I'll try my best to make it happen!" 
-    "\nIf this is your first time playing, please be sure to read the rules before you start.\n" 
+    "\nIf this is your first time playing, please be sure to read the rules before" 
+    "\nyou start.\n" 
     "\nTo read how to play, type 'h2p'.\n"
     "\nTo start the game, type 'play'.\n")
     type_text(intro)
@@ -83,10 +96,79 @@ def main_menu():
             how_to_play()
             break
         else:
-            main_menu_error = "\nI don't know that one... Type 'play' to start the game or 'h2p' to read how to play.\n"
+            main_menu_error = ("\nI don't know that one..." 
+                               "Type 'play' to start the game or 'h2p' to read how to play.\n")
             type_text(main_menu_error)
 
+def how_to_play():
+    """
+    Section to explain the rules to the User and instructions on how to play the game.
+    """
+    how_to_play_intro = ("\nRaven's Rest is a text-based adventure game. You control your" 
+    "\ncharacter by entering commands in the terminal. The goal is to navigate the Raven's" 
+    "\nRest Hotel and find your brother, completing puzzles and defeating enemies as you go.\n"
+    "\nHere are the rules:\n")
+    
+    how_to_play_text = ("\n1. Mind your manners! Don't interrupt your GM. If you type" 
+                        "\nwhilst Poe is typing, you might cause an error, preventing your" 
+                        "\nnext answer from being recognised. Let him finish before typing" 
+                        "\nanything.\n"
+                        "\n2. If you type something that isn't a command, you'll be prompted" 
+                        "\nto try again.\n"
+                        "\n3. You can quit to the main menu at any time by using the 'exit'" 
+                        "\ncommand. But be aware, you'll lose all progress and there's no way to" 
+                        "\nreload a previous game.\n"
+                        "\n4. If you get stuck, type 'help' to see a list of commands you" 
+                        "\ncan use.\n"
+                        "\n5. Once you start the game, you will have to select a character." 
+                        "\nTake note of your character's bio, as their individual skills and" 
+                        "\nexperiences might be useful later on.\n"
+                        "\n6. You can bring up your character's 'Player Card' at any time" 
+                        "\nby using the 'pc' command. Here you'll see your character's current" 
+                        "\nstats and any items in your inventory.\n"
+                        "\n7. The Raven's Rest can be a dangerous place. Your 'Attack Power'" 
+                        "\nstat determines how much damage you can do in a single attack." 
+                        "\nBe sure to pick up any weapons you might find to help you deal" 
+                        "\nmore damage.\n"
+                        "\n8. Keep an eye on your health. If it drops to zero, that's" 
+                        "\ngame over. Healing items can be found and used to keep you going.\n"
+                        "\nThe following are common commands you'll use throughout the game:\n"
+                        "\n'n', 's', 'e' and 'w' to move north, south, east and west, respectively."
+                        "\n'l' to look around the room you're in."
+                        "\n'i' followed by an object to inspect that object."
+                        "\n'u' followed by an object to use that object."
+                        "\n'loot' followed by an object to put that object in your inventory."
+                        "\n'atk' to attack an enemy."
+                        "\n'flee' to flee from a fight, back to the previous room.\n"
+                        "\nThis list is not exhaustive. It just gives examples of common"
+                        "\ncommands you'll use. Remember to use the 'help' command if you get" 
+                        "\nstuck, or consult your Player Card by using the 'pc' command to see if" 
+                        "\nyour character has a skill or item that might be useful.\n"
+                        "\nGood luck, and have fun!\n")
+    
+    how_to_play_text_color = "object"
+    colored_how_to_play_text = how_to_play_text.replace(how_to_play_text_color, f"{Fore.YELLOW}{how_to_play_text_color}{Style.RESET_ALL}")
+    type_text(how_to_play_intro)
+    print(figlet_format("How to Play\n", justify="center"))
+    type_text(colored_how_to_play_text)
+
+    while True:
+        leave_how_to_play = input("\nType 'play' to start the game or 'back' to return to the main menu.\n")
+
+        if leave_how_to_play.lower() == "back":
+            main_menu()
+            break
+        elif leave_how_to_play.lower() == "play":
+            character_selection()
+            break
+        else:
+            leave_how_to_play_error = "\nI don't know that one... Type 'play' to start the game or 'back' to return to the main menu."
+            type_text(leave_how_to_play_error)
+
 def character_selection():
+    """
+    Section where the User must select their Player Character.
+    """
     character_selection_text = ("\nChoose your character:\n"
     "\nLee Kennedy" 
     "\nAge: 36" 
@@ -114,6 +196,9 @@ def character_selection():
             type_text(character_choice_error)     
 
 def start_game():
+    """
+    The game's intro and first area
+    """
     print("\nYou've started the game!")
     print(player_card)
 
